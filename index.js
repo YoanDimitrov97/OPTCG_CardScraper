b7d577006f7fcdcb1ec8859cc5020397c1ab87c6
unknown <yoan.dimitrov97@gmail.com>
2022-08-24 16:46:06 +0300

    OP Tcg Card Scrapper V2, added All cards, waiting for official english translations for card images.

--- index.js
+++ index.js
@@ -1,79 +1,111 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
-const { DownloaderHelper } = require('node-downloader-helper');
+const process = require("process");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const { DownloaderHelper } = require("node-downloader-helper");
 
 (async () => {
+  process.stdout.write("\r Opening Browser.");
   const browser = await puppeteer.launch({
-    headless: true
-  }); // default is true
+    headless: true,
+  }); // default is true, keep it that way unless you want to see what the browser is doing yourself
   const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+
   page.setViewport({
     width: 1366,
-  height: 768,
-  })
+    height: 768,
+  });
   //visit url
-  await page.goto('https://asia-en.onepiece-cardgame.com/cardlist/');
-  page.waitForSelector('.resultCol');//main selector
+  process.stdout.write("\r Visiting Website..");
+  await page.goto("https://asia-en.onepiece-cardgame.com/cardlist/?series=");
+
+  process.stdout.write("\r Loading All Cards...");
+  page.waitForSelector(".resultCol"); //waits for div holder with all cards to load
 
   //get cards
   const obj = await page.evaluate(() => {
-
-  //Split text of Color and Type into an array
+    //Split text of Color and Type into an array
     const SplitTextIntoArr = (card, elClass) => {
       let colorArr = [];
-      card.querySelector(elClass).innerHTML.split('>')[2].split('/').forEach((item) => {
-        colorArr.push(item);
-      }) 
+      card
+        .querySelector(elClass)
+        .innerHTML.split(">")[2]
+        .split("/")
+        .forEach((item) => {
+          colorArr.push(item);
+        });
       return colorArr;
-    }
+    };
 
     //looping through all the cards
-    let cardObj = {'Cards': [{}]};
-    document.querySelectorAll('.modalCol').forEach((card, i) => {
+    let cardObj = { Cards: [{}] };
+    document.querySelectorAll(".modalCol").forEach((card, i) => {
       //need for image saving purposes
-      let cardImageName = card.querySelector('img').getAttribute('src').split('/')[4];
+      let cardImageName = card
+        .querySelector("img")
+        .getAttribute("src")
+        .split("/")[4];
 
       //writing the json
-      cardObj['Cards'][i] = {
-        number: card.querySelector('.infoCol span:first-child').textContent,
-        rarity: card.querySelector('.infoCol span:nth-child(2)').textContent,
-        role: card.querySelector('.infoCol span:last-child').textContent,
-        name: card.querySelector('.cardName').textContent,
-        cost: card.querySelector('.cost').innerHTML.split('>')[2],
-        attribute: (card.querySelector('.attribute i').textContent == "") ? '-' : card.querySelector('.attribute i').textContent,
-        power: card.querySelector('.power').innerHTML.split('>')[2],
-        counter: card.querySelector('.counter').innerHTML.split('>')[2],
-        color: (card.querySelector('.color').innerHTML.split('>')[2].includes('/')) 
-        ? SplitTextIntoArr(card, '.color')
-        : card.querySelector('.color').innerHTML.split('>')[2],
-        type: (card.querySelector('.feature').innerHTML.split('>')[2].includes('/')) 
-        ? SplitTextIntoArr(card, '.feature')
-        : card.querySelector('.feature').innerHTML.split('>')[2],
-        effect: card.querySelector('.text').innerHTML.split('>')[2],
-        set: card.querySelector('.getInfo').innerHTML.split('>')[2],
-        image_name:cardImageName,
-      }
-    })
+      cardObj["Cards"][i] = {
+        number: card.querySelector(".infoCol span:first-child").textContent,
+        rarity: card.querySelector(".infoCol span:nth-child(2)").textContent,
+        role: card.querySelector(".infoCol span:last-child").textContent,
+        name: card.querySelector(".cardName").textContent,
+        cost: card.querySelector(".cost").innerHTML.split(">")[2],
+        attribute:
+          card.querySelector(".attribute i").textContent == ""
+            ? "-"
+            : card.querySelector(".attribute i").textContent,
+        power: card.querySelector(".power").innerHTML.split(">")[2],
+        counter: card.querySelector(".counter").innerHTML.split(">")[2],
+        color: card
+          .querySelector(".color")
+          .innerHTML.split(">")[2]
+          .includes("/")
+          ? SplitTextIntoArr(card, ".color")
+          : card.querySelector(".color").innerHTML.split(">")[2],
+        type: card
+          .querySelector(".feature")
+          .innerHTML.split(">")[2]
+          .includes("/")
+          ? SplitTextIntoArr(card, ".feature")
+          : card.querySelector(".feature").innerHTML.split(">")[2],
+        effect: card.querySelector(".text").innerHTML.split(">")[2],
+        set: card.querySelector(".getInfo").innerHTML.split(">")[2],
+        image_name: cardImageName,
+      };
+    });
     console.log(cardObj);
 
     return cardObj;
-  })
-  console.log(obj[0]);
+  });
+  //console.log(obj[0]);
+  process.stdout.write(`\r Saving to card.json...`);
   const cardData = JSON.stringify(obj);
-  fs.writeFileSync('card.json', cardData);
+  fs.writeFileSync("card.json", cardData);
 
+  process.stdout.write(`\r Downloading images to the img folder...`);
   //#########SAVE IMAGES
-  const getCardImages = await page.$$eval('.modalOpen img', allImg => allImg.map(img => img.src))
+  const getCardImages = await page.$$eval(".modalOpen img", (allImg) =>
+    allImg.map((img) => img.src)
+  );
 
-  getCardImages.map(cardImage => {
+  getCardImages.map((cardImage) => {
     let name = path.basename(cardImage);
-    let dl = new DownloaderHelper(cardImage, path.join(__dirname, '/img'), {fileName:name, override:true, timeout:3500,resumeIfFileExists:true, removeOnStop:false, removeOnFail: false, });
-    dl.on('end', () => console.log('Download Completed'));
-    dl.on('error', (err) => console.log('Download Failed', err));
-    dl.start().catch(err => console.error(err));
-  })
-
+    let dl = new DownloaderHelper(cardImage, path.join(__dirname, "/img"), {
+      fileName: name,
+      override: true,
+      timeout: 3500,
+      resumeIfFileExists: true,
+      removeOnStop: false,
+      removeOnFail: false,
+    });
+    dl.on("end", () => console.log(`${name} saved`));
+    dl.on("error", (err) => console.log("Download Failed", err));
+    dl.start().catch((err) => console.error(err));
+  });
 
   await browser.close();
 })();
